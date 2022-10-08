fdd0a13ed52ce6ca4974980dfe78ac69024206df
Parth Shah <parths1229@gmail.com>
2022-10-09 00:32:22 +0530

    [liter_no.py] add custom input feature

--- liter_no.py
+++ liter_no.py
@@ -1,6 +1,6 @@
 # openssl enc -aes-256-cbc -salt
 
-def solution(n):
+def solution_fn(n):
     d = [0] * 30
     l = 0
     while (n > 0):
@@ -8,7 +8,7 @@ def solution(n):
         n //= 2
         l += 1
 
-    print(d, l, "sdsdds")
+    print(d, l, "what is d and l in this statement?")
     for p in range(1, 1 + l): # L stores strlen,  ut why l+1?
         ok = True
         for i in range(l - p): # doubt on l-p, should be just l
@@ -21,4 +21,5 @@ def solution(n):
     return -1
 
 
-print(solution(955))
+in = input('Enter a number')
+print(solution_fn(int(in)))
