7e173549700cbe76998e682e1425fda2c83a3ce8
Ferrous <busbakka@gmail.com>
2022-10-08 20:59:27 +0530

    Don't remove unhealthy containers:

--- clear_dci.sh
+++ clear_dci.sh
@@ -3,6 +3,5 @@ sudo docker rmi --force registry-ip:5000/frontend
 sudo docker rmi --force frontend
 sudo docker rmi --force registry-ip:5000/todos-api             
 sudo docker rmi --force todos-api                            
-sudo docker rmi --force log-message-processor                
 sudo docker rmi --force registry-ip:5000/log-message-processor 
 sudo docker rmi --force registry-ip:5000/auth-api              
